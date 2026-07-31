191964bebe4ec6a1ad554466cbde383432347f6b
zawthanttun68-sudo <zawthanttun68@gmail.com>
2026-08-01 01:37:05 +0630

    Update script.js

--- script.js
+++ script.js
@@ -230,9 +230,9 @@ function showNextLine(){
 
 }
 const photos = [
-    "images/photo1.jpg",
-    "images/photo2.jpg",
-    "images/photo3.jpg"
+    "photo1.jpg",
+    "photo2.jpg",
+    "photo3.jpg"
 ];
 
 const galleryPage = document.getElementById("galleryPage");
